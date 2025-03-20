ecab5484347673af94db9a892bd041b0fef83789
jason <lemnik@users.noreply.github.com>
2025-03-20 10:58:48 +0000

    chore(mazerunner): fixed linter errors for MazeRunner fixture

--- features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/App.kt
+++ features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/App.kt
@@ -2,13 +2,10 @@ package com.bugsnag.kmp.mazerunner
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
