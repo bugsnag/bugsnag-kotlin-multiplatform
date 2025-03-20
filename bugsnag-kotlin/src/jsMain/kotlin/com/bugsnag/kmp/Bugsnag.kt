b32f68676301608876f4ab95f3666c89f9bb4ac0
YingYing Chen <40571804+YYChen01988@users.noreply.github.com>
2025-03-20 16:28:13 +0000

    feat(Config) add session (#4)

--- bugsnag-kotlin/src/jsMain/kotlin/com/bugsnag/kmp/Bugsnag.kt
+++ bugsnag-kotlin/src/jsMain/kotlin/com/bugsnag/kmp/Bugsnag.kt
@@ -4,4 +4,14 @@ public actual object Bugsnag {
     public actual fun start(configuration: Configuration) {
         JsBugsnag.start(configuration.native)
     }
+
+    public actual fun startSession() {
+        JsBugsnag.startSession()
+    }
+
+    public actual fun pauseSession() {
+        JsBugsnag.pauseSession()
+    }
+
+    public actual fun resumeSession(): Boolean = JsBugsnag.resumeSession()
 }
--- bugsnag-kotlin/src/jsMain/kotlin/com/bugsnag/kmp/JsBugsnag.kt
+++ bugsnag-kotlin/src/jsMain/kotlin/com/bugsnag/kmp/JsBugsnag.kt
@@ -3,4 +3,8 @@ package com.bugsnag.kmp
 @JsModule("@bugsnag/browser")
 internal external object JsBugsnag {
     fun start(apiOrConfig: dynamic)
+
+    fun startSession()
+    fun pauseSession()
+    fun resumeSession(): Boolean
 }
