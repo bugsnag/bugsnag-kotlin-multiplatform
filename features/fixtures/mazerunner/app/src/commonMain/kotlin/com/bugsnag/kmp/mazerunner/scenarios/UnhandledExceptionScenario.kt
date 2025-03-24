0b36117b70ee567e3e3c1f44c6d9304a9b2c5801
jason <lemnik@users.noreply.github.com>
2025-03-24 15:37:34 +0000

    test(mazerunner): add a common/unhandled smoke test

--- features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/scenarios/UnhandledExceptionScenario.kt
+++ features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/scenarios/UnhandledExceptionScenario.kt
@@ -2,8 +2,8 @@ package com.bugsnag.kmp.mazerunner.scenarios
 
 import com.bugsnag.kmp.mazerunner.Scenario
 
-object NotifyScenario : Scenario("NotifyScenario") {
+object UnhandledExceptionScenario : Scenario("UnhandledExceptionScenario") {
     override suspend fun runScenario(config: String) {
-        TODO("Implement Bugsnag.notify and then write this test scenario")
+        throw RuntimeException("this is a large crisis")
     }
 }
