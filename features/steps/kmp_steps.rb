7903cce69de59a6ea3effaf8122a2008fd94296c
jason <lemnik@users.noreply.github.com>
2025-03-25 17:25:04 +0000

    test(mazerunner): added the missing "I relaunch the app after a crash" step

--- features/steps/kmp_steps.rb
+++ features/steps/kmp_steps.rb
@@ -29,6 +29,40 @@ def execute_command(action, scenario_name = '')
 
   raise 'Test fixture did not GET /command' unless Maze::Server.commands.size_remaining == 0
 end
+
+# Waits for up to 10 seconds for the app to stop running.  It seems that Appium doesn't always
+# get the state correct (e.g. when backgrounding the app, or on old Android versions), so we
+# don't fail if it still says running after the time allowed.
+def wait_for_app_state(expected_state)
+  manager = Maze::Api::Appium::AppManager.new
+  max_attempts = 20
+  attempts = 0
+  state = manager.state
+  until (attempts >= max_attempts) || state == expected_state
+    attempts += 1
+    state = manager.state
+    sleep 0.5
+  end
+  $logger.warn "App state #{state} instead of #{expected_state} after 10s" unless state == expected_state
+  state
+end
+
+When("I relaunch the app after a crash") do
+  manager = Maze::Api::Appium::AppManager.new
+  state = wait_for_app_state :not_running
+  if Maze.config.legacy_driver?
+    if state != :not_running
+      manager.close
+    end
+    manager.launch
+  else
+    if state != :not_running
+      manager.terminate
+    end
+    manager.activate
+  end
+end
+
 When('I run {string}') do |scenario_name|
   execute_command :run_scenario, scenario_name
 end
