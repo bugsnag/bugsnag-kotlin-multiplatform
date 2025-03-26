8f9397fc75557e2da0be004ca6927ee84a3cc727
jason <lemnik@users.noreply.github.com>
2025-03-26 14:38:46 +0000

    test(mazerunner): start bugsnag before crashing the UnhandledExceptionScenario

--- features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/scenarios/UnhandledExceptionScenario.kt
+++ features/fixtures/mazerunner/app/src/commonMain/kotlin/com/bugsnag/kmp/mazerunner/scenarios/UnhandledExceptionScenario.kt
@@ -4,6 +4,7 @@ import com.bugsnag.kmp.mazerunner.Scenario
 
 object UnhandledExceptionScenario : Scenario("UnhandledExceptionScenario") {
     override suspend fun runScenario(config: String) {
+        startBugsnag(config)
         throw RuntimeException("this is a large crisis")
     }
 }
