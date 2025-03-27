fc894cddbd9be15b9b574f326dbdf2d719569a6d
jason <lemnik@users.noreply.github.com>
2025-03-27 08:38:53 +0000

    test(mazerunner): start the test fixture on iOS

--- features/fixtures/mazerunner/app/src/iosMain/kotlin/com/bugsnag/kmp/mazerunner/MainViewController.kt
+++ features/fixtures/mazerunner/app/src/iosMain/kotlin/com/bugsnag/kmp/mazerunner/MainViewController.kt
@@ -1,5 +1,12 @@
 package com.bugsnag.kmp.mazerunner
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    App()
+
+    LaunchedEffect(Unit) {
+        MazeRunner.start()
+    }
+}
