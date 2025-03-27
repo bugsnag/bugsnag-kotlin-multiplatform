64795517dcdae1b225fb3cf6e2f36205dd5e021f
jason <lemnik@users.noreply.github.com>
2025-03-27 08:38:53 +0000

    test(mazerunner): start the test fixture on iOS

--- features/fixtures/mazerunner/app/src/iosMain/kotlin/com/bugsnag/kmp/mazerunner/PlatformExtensions.kt
+++ features/fixtures/mazerunner/app/src/iosMain/kotlin/com/bugsnag/kmp/mazerunner/PlatformExtensions.kt
@@ -2,16 +2,17 @@
 
 package com.bugsnag.kmp.mazerunner
 
-import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.toKString
+import kotlinx.cinterop.usePinned
 import platform.Foundation.NSData
 import platform.Foundation.NSError
 import platform.Foundation.NSURLRequest
 import platform.Foundation.NSURLResponse
 import platform.Foundation.NSURLSession
 import platform.Foundation.dataTaskWithRequest
+import platform.posix.memcpy
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -25,10 +26,17 @@ suspend fun NSURLSession.request(urlRequest: NSURLRequest): Pair<NSURLResponse, 
                 cont.resumeWithException(WrappedNSError(error))
             } else if (response != null) {
                 cont.resume(response to data)
+            } else {
+                cont.resumeWithException(NullPointerException("no response or data returned"))
             }
-        }
+        }.resume()
     }
 
-fun NSData.readAsString(): String? {
-    return bytes?.reinterpret<ByteVar>()?.toKString()
+fun NSData.readAsString(): String {
+    val buffer = ByteArray(length.toInt())
+    buffer.usePinned {
+        memcpy(it.addressOf(0), bytes, length)
+    }
+
+    return buffer.toKString()
 }
