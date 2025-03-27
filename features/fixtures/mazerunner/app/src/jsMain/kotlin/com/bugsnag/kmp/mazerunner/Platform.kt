780e90889642344de2c6ed9af4d3c14b3922fe5f
jason <lemnik@users.noreply.github.com>
2025-03-27 13:58:41 +0000

    chore(ci): add browser tests

--- features/fixtures/mazerunner/app/src/jsMain/kotlin/com/bugsnag/kmp/mazerunner/Platform.kt
+++ features/fixtures/mazerunner/app/src/jsMain/kotlin/com/bugsnag/kmp/mazerunner/Platform.kt
@@ -2,11 +2,27 @@ package com.bugsnag.kmp.mazerunner
 
 import com.bugsnag.kmp.Configuration
 import kotlinx.browser.window
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import org.w3c.dom.url.URL
+import kotlin.coroutines.CoroutineContext
 
 actual object Platform {
     private var notifyEndpoint: String? = null
     private var sessionEndpoint: String? = null
+
+    private val windowCoroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        console.log("caught a coroutine exception", throwable)
+        // the only way to get this out of the coroutine stack is to trapeze to a JS timeout
+        window.setTimeout({ throw throwable }, 1)
+    }
+
+    actual val coroutineScope: CoroutineScope = object : CoroutineScope {
+        override val coroutineContext: CoroutineContext
+            get() = windowCoroutineExceptionHandler + SupervisorJob() + Dispatchers.Main
+    }
 
     actual fun configureEndpoints(notify: String, sessions: String) {
         notifyEndpoint = notify
@@ -27,8 +43,10 @@ actual object Platform {
         val pageUrl = URL(window.location.href)
         return Command(
             Command.ACTION_RUN_SCENARIO,
-            requireNotNull(pageUrl.searchParams.get("scenarioName")) { "missing 'scenarioName' query parameter" },
-            pageUrl.searchParams.get("scenarioConfig").orEmpty(),
+            requireNotNull(pageUrl.searchParams.get("SCENARIO")) { "missing 'SCENARIO' query parameter" },
+            pageUrl.searchParams.get("CFG").orEmpty(),
+            pageUrl.searchParams.get("SESSIONS").orEmpty(),
+            pageUrl.searchParams.get("NOTIFY").orEmpty(),
         )
     }
 
