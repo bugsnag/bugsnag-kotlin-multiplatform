940588b6ebb35357a58361377613a644292add63
YingYing Chen <40571804+YYChen01988@users.noreply.github.com>
2025-03-31 08:45:02 +0100

    feat(Config) add context (#11)

--- bugsnag-kotlin/src/commonMain/kotlin/com/bugsnag/kmp/Bugsnag.kt
+++ bugsnag-kotlin/src/commonMain/kotlin/com/bugsnag/kmp/Bugsnag.kt
@@ -2,7 +2,7 @@ package com.bugsnag.kmp
 
 public expect object Bugsnag {
     public fun start(configuration: Configuration)
-    public fun addMetadata( section: String, key: String, value: Any?)
+    public fun addMetadata(section: String, key: String, value: Any?)
     public fun addMetadata(section: String, data: Map<String, Any>)
     public fun startSession()
     public fun pauseSession()
