2b891dc5120d4a24e3d41bca35a09e1913efeb0a
jason <lemnik@users.noreply.github.com>
2025-04-10 16:57:18 +0100

    refactor(js): moved the JS interface definitions into JsBugsnag.kt

--- bugsnag-kmp/src/jsMain/kotlin/com/bugsnag/kmp/JsBugsnag.kt
+++ bugsnag-kmp/src/jsMain/kotlin/com/bugsnag/kmp/JsBugsnag.kt
@@ -18,6 +18,63 @@ internal external object JsBugsnag {
     var user: JsUser
 }
 
+internal external interface JsEndpointConfigurations {
+    var notify: String
+    var sessions: String
+}
+
+internal fun JsEndpointConfigurations(notify: String, sessions: String): JsEndpointConfigurations {
+    val endpoints = Any().unsafeCast<JsEndpointConfigurations>()
+    endpoints.notify = notify
+    endpoints.sessions = sessions
+    return endpoints
+}
+
+internal external interface JsFeatureFlag {
+    var name: String
+    var variant: String?
+}
+
+internal fun JsFeatureFlag(name: String, variant: String?): JsFeatureFlag {
+    val flag = Any().unsafeCast<JsFeatureFlag>()
+    if (variant != null) {
+        flag.name = name
+        flag.variant = variant
+    } else {
+        flag.name = name
+    }
+    return flag
+}
+
+internal external interface JsUser {
+    var id: String?
+    var name: String?
+    var email: String?
+}
+
+internal fun JsUser(id: String?, email: String?, name: String?): JsUser {
+    val user = Any().unsafeCast<JsUser>()
+    id?.let { user.id = it }
+    email?.let { user.email = it }
+    name?.let { user.name = it }
+    return user
+}
+
+internal external interface JsErrorTypes {
+    var unhandledExceptions: Boolean?
+    var unhandledRejections: Boolean?
+}
+
+internal fun JsErrorTypes(
+    unhandledExceptions: Boolean?,
+    unhandledRejections: Boolean?
+): JsErrorTypes {
+    val errorTypes = Any().unsafeCast<JsErrorTypes>()
+    errorTypes.unhandledExceptions = unhandledExceptions
+    errorTypes.unhandledRejections = unhandledRejections
+    return errorTypes
+}
+
 internal fun <V> Map<*, V>.convertToDynamic(): dynamic {
     val dynamicData = Any().asDynamic()
     forEach { (key, value) ->
