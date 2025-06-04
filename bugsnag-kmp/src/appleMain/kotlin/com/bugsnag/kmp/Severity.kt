59cfead958be589f9a70d0ef4eae40fffe3a24b1
YYChen01988 <Chen.yingying.01988@gmail.com>
2025-06-04 17:42:40 +0100

    feat(event) add event class and ErrorCallback

---
+++ bugsnag-kmp/src/appleMain/kotlin/com/bugsnag/kmp/Severity.kt
@@ -0,0 +1,23 @@
+package com.bugsnag.kmp
+
+import kotlinx.cinterop.ExperimentalForeignApi
+import com.bugsnag.cocoa.BSGSeverity as AppleSeverity
+
+@OptIn(ExperimentalForeignApi::class)
+internal fun Severity.toNativePlatformType(): AppleSeverity = when (this) {
+    Severity.ERROR -> AppleSeverity.BSGSeverityError
+    Severity.INFO -> AppleSeverity.BSGSeverityInfo
+    Severity.WARNING -> AppleSeverity.BSGSeverityWarning
+}
+
+@OptIn(ExperimentalForeignApi::class)
+internal fun AppleSeverity.toPlatformType(): Severity = when (this) {
+    AppleSeverity.BSGSeverityError -> Severity.ERROR
+    AppleSeverity.BSGSeverityInfo -> Severity.INFO
+    AppleSeverity.BSGSeverityWarning -> Severity.WARNING
+
+    // TODO This is not correct
+    else -> {
+        Severity.ERROR
+    }
+}
